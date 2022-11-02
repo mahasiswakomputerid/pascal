7332d0394d48cb80c1c0bee861f4e546ec6c7a60
mahasiswakomputerid <106739559+mahasiswakomputerid@users.noreply.github.com>
2022-11-02 18:49:12 +0700

    Create if1k1.pas

---
+++ if1k1.pas
@@ -0,0 +1,14 @@
+Program if1;
+Uses CRT;
+Var a, b : Integer;
+Begin
+   ClrScr;
+    Writeln('Isi nilai A :');
+   read(a);
+    Writeln('Isi nilai B :');
+   read(b);
+   if (a >b) then
+      Writeln('Bilangan A terbesar')
+   else
+      Writeln('Bilangan B terbesar');
+end.
