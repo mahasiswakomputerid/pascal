924ba1f4cb36113b9b31c3f2477699a225e94bff
mahasiswakomputerid <106739559+mahasiswakomputerid@users.noreply.github.com>
2023-07-26 01:14:22 +0700

    Create 1-Contoh_Awal.pas

---
+++ 1-Contoh_Awal.pas
@@ -0,0 +1,11 @@
+Program Contoh_Awal;
+Uses CRT;
+Var i,j,k : integer;
+Begin
+  ClrScr;
+  Write('masukkan nilai i dan j : ');
+  Readln(i,j);
+  k:=i+j;
+  Writeln('nilai k : ',k);
+  Readln;
+End.
